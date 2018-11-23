8a33797463ab28b23397f73d56d1a4d990874c17
Kevin Mahoney <kmahoney40@gmail.com>
2018-11-22 20:31:51 -0800

    Added a little error handling.

--- src/components/button-with-confirmation.js
+++ src/components/button-with-confirmation.js
@@ -3,7 +3,7 @@ import './../index.css';
 import Button from 'react-bootstrap/lib/Button'
 import Modal from 'react-bootstrap/lib/Modal'
 import './../../node_modules/bootstrap/dist/css/bootstrap.min.css';
-
+import axios from 'axios';
 
 class ButtonWithConfirmation extends React.Component{
   constructor(props, context) {
@@ -14,7 +14,8 @@ class ButtonWithConfirmation extends React.Component{
     this.handleYes = this.handleYes.bind(this);
 
     this.state = {
-      show: false
+      show: false,
+      errMsg: ''
     };
   }
 
@@ -23,7 +24,22 @@ class ButtonWithConfirmation extends React.Component{
   }
 
   handleYes(){
-    this.setState({ show: false });
+    this.postDoor();
+    // this.setState({ show: false });
+  }
+
+  postDoor(){
+    axios.post('http://52.9.238.232/api/do0r?cmd=true')
+    .then(function(response){
+      console.log(response.status);
+      if(response.status === 200){
+        this.setState({ show: false });
+      } else {
+        console.log(response.status);
+        this.setState({ errMsg: response.status + ' ' + response.message })
+      }
+    })
+    .catch(err => {console.log(err); this.setState({ errMsg: err.message + ': Try again?' })});
   }
 
   handleShow() {
@@ -33,10 +49,8 @@ class ButtonWithConfirmation extends React.Component{
   render(){
     return (
       <div>
-        <p>Click to get the full Modal experience!</p>
-
         <Button bsstyle="primary" bssize="large" onClick={this.handleShow}>
-          Launch demo modal
+          Toggel Door
         </Button>
 
         <Modal className="static-modal" show={this.state.show} onHide={this.handleClose}>
@@ -44,6 +58,7 @@ class ButtonWithConfirmation extends React.Component{
             <Modal.Title>Toggle Door?</Modal.Title>
           </Modal.Header>
           <Modal.Footer>
+            <label>{this.state.errMsg}</label>
             <Button className="btn-warning" onClick={this.handleYes}>Yes</Button>
             <Button onClick={this.handleClose}>No</Button>
           </Modal.Footer>
